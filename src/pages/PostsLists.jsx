6f049d13759a110f6fb80160240366790dd9e7ab
PierdomenicoPacilio <piero.pacilio@gmail.com>
2025-04-16 13:51:12 +0200

    feat: posts list from api

--- src/pages/PostsLists.jsx
+++ src/pages/PostsLists.jsx
@@ -1,3 +1,22 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 export default function PostsList() {
-    return <h2>Lista dei Post</h2>
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(element =>
+                setPosts(element.data)
+            )
+            .catch(error =>
+                console.log(error)
+            )
+    }, [])
+
+    return <ul>
+        {posts.map(post => <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+        </li>)}
+    </ul>
 }
